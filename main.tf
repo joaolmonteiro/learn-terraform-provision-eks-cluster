df4cdda8ce39b2bff426f1ec6e501739dd0445c0
Joao Monteiro <joaolmonteiro94@gmail.com>
2025-09-04 11:32:27 +0100

    disable cluster

--- main.tf
+++ main.tf
@@ -72,27 +72,27 @@ module "eks" {
 
   }
 
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
+#  eks_managed_node_groups = {
+#    one = {
+#      name = "node-group-1"
+#
+#      instance_types = ["t3.small"]
+#
+#      min_size     = 1
+#      max_size     = 2
+#      desired_size = 1
+#    }
+#
+#    two = {
+#      name = "node-group-2"
+#
+#      instance_types = ["t3.small"]
+#
+#      min_size     = 1
+#      max_size     = 2
+#      desired_size = 1
+#    }
+#  }
 }
 
 
